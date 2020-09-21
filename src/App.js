1ac43f2d63cdde11972017478ec24edb364624b6
joez <joe@fred.com>
2020-09-21 13:30:43 -0600

    uncaught changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
-import { DISHES } from './shared/dishes'
-import { Navbar,NavbarBrand } from 'reactstrap';
+import Main from './components/Main';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      dishes: DISHES
-    };
-    
-  }
   render () {
     return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes} />
+        <Main />
     </div>
     )
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Menu';
 import Header from './Header';
 import Footer from './Footer';
