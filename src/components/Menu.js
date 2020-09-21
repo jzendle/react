4fd16bb577d5e2d50732c0906c8c1ebee5d22d77
joez <joe@fred.com>
2020-09-21 13:04:53 -0600

    container components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap'
-import DishDetail from '../components/DishDetail';
 
 class Menu extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDish: null
-    }
-    console.log('ctor')
-    console.log(this)
-  }
 
   setSelected = (selection) => {
     this.setState({ selectedDish: selection })
@@ -24,7 +15,7 @@ class Menu extends Component {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => { console.log(dish); this.setSelected(dish) }}>
+          <Card onClick={() => this.props.onClick(dish.id)  }>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -40,7 +31,6 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
-        <DishDetail dish={ this.state.selectedDish } />
       </div>
     );
   }
