15f48a7dcaca24156dabc9a7f7996d5030076e43
joez <joe@fred.com>
2020-09-29 17:56:13 -0600

    spa

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,44 +1,55 @@
 import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
+const RenderDish = (props) => {
+    return (
+        <Card>
+            <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
+            <CardBody>
+                <CardTitle>{props.dish.name}</CardTitle>
+                <CardText>{props.dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+const RenderComments = (props) => {
+    return (
+        props.comments.map(
+            (comment) => {
+            return (
+                <div key={comment.id}> <p> {comment.comment}</p></div>
+            )
+        }
+        ) // map
+     ) // return
+}
 
 const DishDetail = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
 
-
-    let ret = <div className="row"></div>;
-    if (!props.dish) // short circuit if there is no dish
-        return ret;
-
-
-    let review_txt = props.dish.reviews.map(
-        (review) => {
-            return (<div key={review.id} >
-                <p> {review.text} </p>
-            </div>)
-        }
-    )
-    if (!review_txt || 0 === review_txt.length)
-        review_txt = <div>no reviews yet. </div>;
-
-    ret =
-        <div className="row">
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
-                    <CardBody>
-                        <CardTitle>{props.dish.name}</CardTitle>
-                        <CardText>{props.dish.description}</CardText>
-                    </CardBody>
-
-                </Card>
-            </div >
-            <div className="col col-md-5 m-1">
-                <h1>Customer Reviews</h1>
-                {review_txt}
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments} />
+                </div>
             </div>
         </div>
-
-    return ret;
-};
+    )
+}
 
 export default DishDetail;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import Menu from './Menu';
 import Home from './Home';
 import Header from './Header';
+import Contact from './Contact';
 import Footer from './Footer';
+import DishDetail from './DishDetail';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,24 +18,38 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
-      return (
-        <Home />
-      )
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path="/menu" 
-            component={() => <Menu dishes={this.state.dishes} />} />
-          <Redirect to="home" />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
         </Switch>
         <Footer />
       </div>
